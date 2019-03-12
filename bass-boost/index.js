ad49262c3eb61b6a526874c02ff38a5d62da3c92
Vladislav Yuschuk <vladislav.yuschuk@nixsolutions.com>
2019-03-12 23:25:57 +0200

    fixes for bot

--- bass-boost/index.js
+++ bass-boost/index.js
@@ -1,22 +1,27 @@
 // var sox = require('sox.js');
 
-var exec = require('child_process').exec;
+var exec = require("child_process").exec;
+var sox = require("sox.js");
+module.exports = function(audioFile, outputFileName, settings = "bass +15") {
+  var resolve = null;
 
-module.exports = function (audiofile, outputfilename, settings) {
-    var resolve = null;
+  var promise = new Promise(r => {
+    resolve = r;
+  });
 
-    var promise = new Promise((r) => { resolve = r });
+  var defaultSettings = "bass +15 speed 0.95";
+  settings = settings || defaultSettings;
 
-    var defaultSettings = 'bass +15 speed 0.95';
-    settings = settings || defaultSettings;
+  exec(
+    `sox -G ${audioFile} ${outputFileName} ${settings}`,
+    { maxBuffer: 1024 * 200 },
+    function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      resolve();
+    }
+  );
 
-    exec(`sox -G ${audiofile} ${outputfilename} ${settings}`, {maxBuffer: 1024 * 200}, function(err) {
-        if (err) {
-            return console.log(err);
-        }
-
-        resolve();
-    })
-
-    return promise;
-}
+  return promise;
+};
